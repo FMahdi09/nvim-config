a75d418c6cbab9a3711e3835c42058343d5dcadd
Fabian Mahdi <Fabian@Mahdi.at>
2025-03-02 15:26:07 +0800

    latest

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -12,6 +12,7 @@ return {
 				ensure_installed = {
 					"lua_ls",
 					"pyright",
+					"ts_ls",
 				},
 			})
 		end,
@@ -22,6 +23,13 @@ return {
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({})
 			lspconfig.pyright.setup({})
+			lspconfig.ts_ls.setup({
+				init_options = {
+					preferences = {
+						disableSuggestions = true,
+					},
+				},
+			})
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
